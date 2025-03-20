539f9e08f859dbf8d75d057598c9b3b425063953
OS Guy <kocabasmestan@gmail.com>
2025-03-20 23:15:10 +0300

    Added some minimal keybinds for direct screenshot

---
+++ .config/hypr/scripts/screenshot_area.sh
@@ -0,0 +1,9 @@
+THIS IS A PART OF THE screenshot_shot-record.sh FILE
+
+get_geometry() {
+    slurp "$@"
+}
+
+geometry=$(get_geometry)
+grim -g "$geometry" - | wl-copy
+notify-send "Screenshot Copied" "Area screenshot copied to clipboard"
